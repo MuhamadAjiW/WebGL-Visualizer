2e5f9b9f1e2fc648d81bbd0e5f45f141c8f0f976
Fakhri Mahendra <13521045@mahasiswa.itb.ac.id>
2024-03-25 20:02:10 +0700

    test: test git commit

--- src/models/line-model.ts
+++ src/models/line-model.ts
@@ -4,6 +4,7 @@ export class LineModel extends BaseModel {
     constructor(gl : WebGLRenderingContext) {
         super(gl);
     }
+    
 
     draw(): void {
         throw new Error("Method not implemented.");
