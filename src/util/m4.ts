ddbb19d9c3558db1668a572fca854bc3f2ae108d
Fakhrimm <13521045@mahasiswa.itb.ac.id>
2024-04-03 19:01:55 +0700

    feat: export m4.js to m4.ts

---
+++ src/util/m4.ts
@@ -0,0 +1,613 @@
+type Vector3 = [number, number, number];
+type Vector4 = [number, number, number, number];
+type Matrix4 = [
+    number,
+    number,
+    number,
+    number,
+    number,
+    number,
+    number,
+    number,
+    number,
+    number,
+    number,
+    number,
+    number,
+    number,
+    number,
+    number
+];
+
+let MatType: new (size: number) => Float32Array | Array<number> = Float32Array;
+
+export const m4 = {
+    multiply: function(a: Matrix4, b: Matrix4, dst?: Matrix4): Matrix4 {
+    dst = dst || (new MatType(16) as Matrix4);
+    const b00 = b[0 * 4 + 0];
+    const b01 = b[0 * 4 + 1];
+    const b02 = b[0 * 4 + 2];
+    const b03 = b[0 * 4 + 3];
+    const b10 = b[1 * 4 + 0];
+    const b11 = b[1 * 4 + 1];
+    const b12 = b[1 * 4 + 2];
+    const b13 = b[1 * 4 + 3];
+    const b20 = b[2 * 4 + 0];
+    const b21 = b[2 * 4 + 1];
+    const b22 = b[2 * 4 + 2];
+    const b23 = b[2 * 4 + 3];
+    const b30 = b[3 * 4 + 0];
+    const b31 = b[3 * 4 + 1];
+    const b32 = b[3 * 4 + 2];
+    const b33 = b[3 * 4 + 3];
+    const a00 = a[0 * 4 + 0];
+    const a01 = a[0 * 4 + 1];
+    const a02 = a[0 * 4 + 2];
+    const a03 = a[0 * 4 + 3];
+    const a10 = a[1 * 4 + 0];
+    const a11 = a[1 * 4 + 1];
+    const a12 = a[1 * 4 + 2];
+    const a13 = a[1 * 4 + 3];
+    const a20 = a[2 * 4 + 0];
+    const a21 = a[2 * 4 + 1];
+    const a22 = a[2 * 4 + 2];
+    const a23 = a[2 * 4 + 3];
+    const a30 = a[3 * 4 + 0];
+    const a31 = a[3 * 4 + 1];
+    const a32 = a[3 * 4 + 2];
+    const a33 = a[3 * 4 + 3];
+    dst[0] = b00 * a00 + b01 * a10 + b02 * a20 + b03 * a30;
+    dst[1] = b00 * a01 + b01 * a11 + b02 * a21 + b03 * a31;
+    dst[2] = b00 * a02 + b01 * a12 + b02 * a22 + b03 * a32;
+    dst[3] = b00 * a03 + b01 * a13 + b02 * a23 + b03 * a33;
+    dst[4] = b10 * a00 + b11 * a10 + b12 * a20 + b13 * a30;
+    dst[5] = b10 * a01 + b11 * a11 + b12 * a21 + b13 * a31;
+    dst[6] = b10 * a02 + b11 * a12 + b12 * a22 + b13 * a32;
+    dst[7] = b10 * a03 + b11 * a13 + b12 * a23 + b13 * a33;
+    dst[8] = b20 * a00 + b21 * a10 + b22 * a20 + b23 * a30;
+    dst[9] = b20 * a01 + b21 * a11 + b22 * a21 + b23 * a31;
+    dst[10] = b20 * a02 + b21 * a12 + b22 * a22 + b23 * a32;
+    dst[11] = b20 * a03 + b21 * a13 + b22 * a23 + b23 * a33;
+    dst[12] = b30 * a00 + b31 * a10 + b32 * a20 + b33 * a30;
+    dst[13] = b30 * a01 + b31 * a11 + b32 * a21 + b33 * a31;
+    dst[14] = b30 * a02 + b31 * a12 + b32 * a22 + b33 * a32;
+    dst[15] = b30 * a03 + b31 * a13 + b32 * a23 + b33 * a33;
+    return dst;
+},
+
+transpose: function(m: Matrix4, dst: Matrix4): Matrix4 {
+    dst = dst || new MatType(16);
+
+    dst[ 0] = m[0];
+    dst[ 1] = m[4];
+    dst[ 2] = m[8];
+    dst[ 3] = m[12];
+    dst[ 4] = m[1];
+    dst[ 5] = m[5];
+    dst[ 6] = m[9];
+    dst[ 7] = m[13];
+    dst[ 8] = m[2];
+    dst[ 9] = m[6];
+    dst[10] = m[10];
+    dst[11] = m[14];
+    dst[12] = m[3];
+    dst[13] = m[7];
+    dst[14] = m[11];
+    dst[15] = m[15];
+
+    return dst;
+  },
+  /**
+     * Makes a translation matrix
+     * @param {number} tx x translation.
+     * @param {number} ty y translation.
+     * @param {number} tz z translation.
+     * @param {Matrix4} [dst] optional matrix to store result
+     * @return {Matrix4} dst or a new matrix if none provided
+     * @memberOf module:webgl-3d-math
+     */
+  translation: function(tx: number, ty: number, tz: number, dst: Matrix4): Matrix4 {
+    dst = dst || new MatType(16);
+
+    dst[ 0] = 1;
+    dst[ 1] = 0;
+    dst[ 2] = 0;
+    dst[ 3] = 0;
+    dst[ 4] = 0;
+    dst[ 5] = 1;
+    dst[ 6] = 0;
+    dst[ 7] = 0;
+    dst[ 8] = 0;
+    dst[ 9] = 0;
+    dst[10] = 1;
+    dst[11] = 0;
+    dst[12] = tx;
+    dst[13] = ty;
+    dst[14] = tz;
+    dst[15] = 1;
+
+    return dst;
+  },
+
+  /**
+   * Multiply by translation matrix.
+   * @param {Matrix4} m matrix to multiply
+   * @param {number} tx x translation.
+   * @param {number} ty y translation.
+   * @param {number} tz z translation.
+   * @param {Matrix4} [dst] optional matrix to store result
+   * @return {Matrix4} dst or a new matrix if none provided
+   * @memberOf module:webgl-3d-math
+   */
+  translate: function(m: Matrix4, tx: number, ty: number, tz: number, dst: Matrix4): Matrix4 {
+    // This is the optimized version of
+    // return multiply(m, translation(tx, ty, tz), dst);
+    dst = dst || new MatType(16);
+
+    var m00 = m[0];
+    var m01 = m[1];
+    var m02 = m[2];
+    var m03 = m[3];
+    var m10 = m[1 * 4 + 0];
+    var m11 = m[1 * 4 + 1];
+    var m12 = m[1 * 4 + 2];
+    var m13 = m[1 * 4 + 3];
+    var m20 = m[2 * 4 + 0];
+    var m21 = m[2 * 4 + 1];
+    var m22 = m[2 * 4 + 2];
+    var m23 = m[2 * 4 + 3];
+    var m30 = m[3 * 4 + 0];
+    var m31 = m[3 * 4 + 1];
+    var m32 = m[3 * 4 + 2];
+    var m33 = m[3 * 4 + 3];
+
+    if (m !== dst) {
+      dst[ 0] = m00;
+      dst[ 1] = m01;
+      dst[ 2] = m02;
+      dst[ 3] = m03;
+      dst[ 4] = m10;
+      dst[ 5] = m11;
+      dst[ 6] = m12;
+      dst[ 7] = m13;
+      dst[ 8] = m20;
+      dst[ 9] = m21;
+      dst[10] = m22;
+      dst[11] = m23;
+    }
+
+    dst[12] = m00 * tx + m10 * ty + m20 * tz + m30;
+    dst[13] = m01 * tx + m11 * ty + m21 * tz + m31;
+    dst[14] = m02 * tx + m12 * ty + m22 * tz + m32;
+    dst[15] = m03 * tx + m13 * ty + m23 * tz + m33;
+
+    return dst;
+  },
+
+  /**
+   * Makes an x rotation matrix
+   * @param {number} angleInRadians amount to rotate
+   * @param {Matrix4} [dst] optional matrix to store result
+   * @return {Matrix4} dst or a new matrix if none provided
+   * @memberOf module:webgl-3d-math
+   */
+  xRotation: function(angleInRadians: number, dst: Matrix4): Matrix4 {
+    dst = dst || new MatType(16);
+    var c = Math.cos(angleInRadians);
+    var s = Math.sin(angleInRadians);
+
+    dst[ 0] = 1;
+    dst[ 1] = 0;
+    dst[ 2] = 0;
+    dst[ 3] = 0;
+    dst[ 4] = 0;
+    dst[ 5] = c;
+    dst[ 6] = s;
+    dst[ 7] = 0;
+    dst[ 8] = 0;
+    dst[ 9] = -s;
+    dst[10] = c;
+    dst[11] = 0;
+    dst[12] = 0;
+    dst[13] = 0;
+    dst[14] = 0;
+    dst[15] = 1;
+
+    return dst;
+  },
+
+  /**
+   * Multiply by an x rotation matrix
+   * @param {Matrix4} m matrix to multiply
+   * @param {number} angleInRadians amount to rotate
+   * @param {Matrix4} [dst] optional matrix to store result
+   * @return {Matrix4} dst or a new matrix if none provided
+   * @memberOf module:webgl-3d-math
+   */
+  xRotate: function(m: Matrix4, angleInRadians: number, dst: Matrix4): Matrix4 {
+    // this is the optimized version of
+    // return multiply(m, xRotation(angleInRadians), dst);
+    dst = dst || new MatType(16);
+
+    var m10 = m[4];
+    var m11 = m[5];
+    var m12 = m[6];
+    var m13 = m[7];
+    var m20 = m[8];
+    var m21 = m[9];
+    var m22 = m[10];
+    var m23 = m[11];
+    var c = Math.cos(angleInRadians);
+    var s = Math.sin(angleInRadians);
+
+    dst[4]  = c * m10 + s * m20;
+    dst[5]  = c * m11 + s * m21;
+    dst[6]  = c * m12 + s * m22;
+    dst[7]  = c * m13 + s * m23;
+    dst[8]  = c * m20 - s * m10;
+    dst[9]  = c * m21 - s * m11;
+    dst[10] = c * m22 - s * m12;
+    dst[11] = c * m23 - s * m13;
+
+    if (m !== dst) {
+      dst[ 0] = m[ 0];
+      dst[ 1] = m[ 1];
+      dst[ 2] = m[ 2];
+      dst[ 3] = m[ 3];
+      dst[12] = m[12];
+      dst[13] = m[13];
+      dst[14] = m[14];
+      dst[15] = m[15];
+    }
+
+    return dst;
+  },
+
+  /**
+   * Makes an y rotation matrix
+   * @param {number} angleInRadians amount to rotate
+   * @param {Matrix4} [dst] optional matrix to store result
+   * @return {Matrix4} dst or a new matrix if none provided
+   * @memberOf module:webgl-3d-math
+   */
+  yRotation: function(angleInRadians: number, dst: Matrix4): Matrix4 {
+    dst = dst || new MatType(16);
+    var c = Math.cos(angleInRadians);
+    var s = Math.sin(angleInRadians);
+
+    dst[ 0] = c;
+    dst[ 1] = 0;
+    dst[ 2] = -s;
+    dst[ 3] = 0;
+    dst[ 4] = 0;
+    dst[ 5] = 1;
+    dst[ 6] = 0;
+    dst[ 7] = 0;
+    dst[ 8] = s;
+    dst[ 9] = 0;
+    dst[10] = c;
+    dst[11] = 0;
+    dst[12] = 0;
+    dst[13] = 0;
+    dst[14] = 0;
+    dst[15] = 1;
+
+    return dst;
+  },
+
+  /**
+   * Multiply by an y rotation matrix
+   * @param {Matrix4} m matrix to multiply
+   * @param {number} angleInRadians amount to rotate
+   * @param {Matrix4} [dst] optional matrix to store result
+   * @return {Matrix4} dst or a new matrix if none provided
+   * @memberOf module:webgl-3d-math
+   */
+  yRotate: function(m: Matrix4, angleInRadians: number, dst: Matrix4): Matrix4 {
+    // this is the optimized version of
+    // return multiply(m, yRotation(angleInRadians), dst);
+    dst = dst || new MatType(16);
+
+    var m00 = m[0 * 4 + 0];
+    var m01 = m[0 * 4 + 1];
+    var m02 = m[0 * 4 + 2];
+    var m03 = m[0 * 4 + 3];
+    var m20 = m[2 * 4 + 0];
+    var m21 = m[2 * 4 + 1];
+    var m22 = m[2 * 4 + 2];
+    var m23 = m[2 * 4 + 3];
+    var c = Math.cos(angleInRadians);
+    var s = Math.sin(angleInRadians);
+
+    dst[ 0] = c * m00 - s * m20;
+    dst[ 1] = c * m01 - s * m21;
+    dst[ 2] = c * m02 - s * m22;
+    dst[ 3] = c * m03 - s * m23;
+    dst[ 8] = c * m20 + s * m00;
+    dst[ 9] = c * m21 + s * m01;
+    dst[10] = c * m22 + s * m02;
+    dst[11] = c * m23 + s * m03;
+
+    if (m !== dst) {
+      dst[ 4] = m[ 4];
+      dst[ 5] = m[ 5];
+      dst[ 6] = m[ 6];
+      dst[ 7] = m[ 7];
+      dst[12] = m[12];
+      dst[13] = m[13];
+      dst[14] = m[14];
+      dst[15] = m[15];
+    }
+
+    return dst;
+  },
+
+  /**
+   * Makes an z rotation matrix
+   * @param {number} angleInRadians amount to rotate
+   * @param {Matrix4} [dst] optional matrix to store result
+   * @return {Matrix4} dst or a new matrix if none provided
+   * @memberOf module:webgl-3d-math
+   */
+  zRotation: function(angleInRadians: number, dst: Matrix4): Matrix4 {
+    dst = dst || new MatType(16);
+    var c = Math.cos(angleInRadians);
+    var s = Math.sin(angleInRadians);
+
+    dst[ 0] = c;
+    dst[ 1] = s;
+    dst[ 2] = 0;
+    dst[ 3] = 0;
+    dst[ 4] = -s;
+    dst[ 5] = c;
+    dst[ 6] = 0;
+    dst[ 7] = 0;
+    dst[ 8] = 0;
+    dst[ 9] = 0;
+    dst[10] = 1;
+    dst[11] = 0;
+    dst[12] = 0;
+    dst[13] = 0;
+    dst[14] = 0;
+    dst[15] = 1;
+
+    return dst;
+  },
+
+  /**
+   * Multiply by an z rotation matrix
+   * @param {Matrix4} m matrix to multiply
+   * @param {number} angleInRadians amount to rotate
+   * @param {Matrix4} [dst] optional matrix to store result
+   * @return {Matrix4} dst or a new matrix if none provided
+   * @memberOf module:webgl-3d-math
+   */
+  zRotate: function(m: Matrix4, angleInRadians: number, dst: Matrix4) {
+    // This is the optimized version of
+    // return multiply(m, zRotation(angleInRadians), dst);
+    dst = dst || new MatType(16);
+
+    var m00 = m[0 * 4 + 0];
+    var m01 = m[0 * 4 + 1];
+    var m02 = m[0 * 4 + 2];
+    var m03 = m[0 * 4 + 3];
+    var m10 = m[1 * 4 + 0];
+    var m11 = m[1 * 4 + 1];
+    var m12 = m[1 * 4 + 2];
+    var m13 = m[1 * 4 + 3];
+    var c = Math.cos(angleInRadians);
+    var s = Math.sin(angleInRadians);
+
+    dst[ 0] = c * m00 + s * m10;
+    dst[ 1] = c * m01 + s * m11;
+    dst[ 2] = c * m02 + s * m12;
+    dst[ 3] = c * m03 + s * m13;
+    dst[ 4] = c * m10 - s * m00;
+    dst[ 5] = c * m11 - s * m01;
+    dst[ 6] = c * m12 - s * m02;
+    dst[ 7] = c * m13 - s * m03;
+
+    if (m !== dst) {
+      dst[ 8] = m[ 8];
+      dst[ 9] = m[ 9];
+      dst[10] = m[10];
+      dst[11] = m[11];
+      dst[12] = m[12];
+      dst[13] = m[13];
+      dst[14] = m[14];
+      dst[15] = m[15];
+    }
+
+    return dst;
+  },
+  /**
+     * Makes an rotation matrix around an arbitrary axis
+     * @param {Vector3} axis axis to rotate around
+     * @param {number} angleInRadians amount to rotate
+     * @param {Matrix4} [dst] optional matrix to store result
+     * @return {Matrix4} dst or a new matrix if none provided
+     * @memberOf module:webgl-3d-math
+     */
+  axisRotation: function(axis: Vector3, angleInRadians: number, dst: Matrix4): Matrix4 {
+    dst = dst || new MatType(16);
+
+    var x = axis[0];
+    var y = axis[1];
+    var z = axis[2];
+    var n = Math.sqrt(x * x + y * y + z * z);
+    x /= n;
+    y /= n;
+    z /= n;
+    var xx = x * x;
+    var yy = y * y;
+    var zz = z * z;
+    var c = Math.cos(angleInRadians);
+    var s = Math.sin(angleInRadians);
+    var oneMinusCosine = 1 - c;
+
+    dst[ 0] = xx + (1 - xx) * c;
+    dst[ 1] = x * y * oneMinusCosine + z * s;
+    dst[ 2] = x * z * oneMinusCosine - y * s;
+    dst[ 3] = 0;
+    dst[ 4] = x * y * oneMinusCosine - z * s;
+    dst[ 5] = yy + (1 - yy) * c;
+    dst[ 6] = y * z * oneMinusCosine + x * s;
+    dst[ 7] = 0;
+    dst[ 8] = x * z * oneMinusCosine + y * s;
+    dst[ 9] = y * z * oneMinusCosine - x * s;
+    dst[10] = zz + (1 - zz) * c;
+    dst[11] = 0;
+    dst[12] = 0;
+    dst[13] = 0;
+    dst[14] = 0;
+    dst[15] = 1;
+
+    return dst;
+  },
+
+  /**
+   * Multiply by an axis rotation matrix
+   * @param {Matrix4} m matrix to multiply
+   * @param {Vector3} axis axis to rotate around
+   * @param {number} angleInRadians amount to rotate
+   * @param {Matrix4} [dst] optional matrix to store result
+   * @return {Matrix4} dst or a new matrix if none provided
+   * @memberOf module:webgl-3d-math
+   */
+  axisRotate: function(m: Matrix4, axis: Vector3, angleInRadians: number, dst: Matrix4) {
+    // This is the optimized version of
+    // return multiply(m, axisRotation(axis, angleInRadians), dst);
+    dst = dst || new MatType(16);
+
+    var x = axis[0];
+    var y = axis[1];
+    var z = axis[2];
+    var n = Math.sqrt(x * x + y * y + z * z);
+    x /= n;
+    y /= n;
+    z /= n;
+    var xx = x * x;
+    var yy = y * y;
+    var zz = z * z;
+    var c = Math.cos(angleInRadians);
+    var s = Math.sin(angleInRadians);
+    var oneMinusCosine = 1 - c;
+
+    var r00 = xx + (1 - xx) * c;
+    var r01 = x * y * oneMinusCosine + z * s;
+    var r02 = x * z * oneMinusCosine - y * s;
+    var r10 = x * y * oneMinusCosine - z * s;
+    var r11 = yy + (1 - yy) * c;
+    var r12 = y * z * oneMinusCosine + x * s;
+    var r20 = x * z * oneMinusCosine + y * s;
+    var r21 = y * z * oneMinusCosine - x * s;
+    var r22 = zz + (1 - zz) * c;
+
+    var m00 = m[0];
+    var m01 = m[1];
+    var m02 = m[2];
+    var m03 = m[3];
+    var m10 = m[4];
+    var m11 = m[5];
+    var m12 = m[6];
+    var m13 = m[7];
+    var m20 = m[8];
+    var m21 = m[9];
+    var m22 = m[10];
+    var m23 = m[11];
+
+    dst[ 0] = r00 * m00 + r01 * m10 + r02 * m20;
+    dst[ 1] = r00 * m01 + r01 * m11 + r02 * m21;
+    dst[ 2] = r00 * m02 + r01 * m12 + r02 * m22;
+    dst[ 3] = r00 * m03 + r01 * m13 + r02 * m23;
+    dst[ 4] = r10 * m00 + r11 * m10 + r12 * m20;
+    dst[ 5] = r10 * m01 + r11 * m11 + r12 * m21;
+    dst[ 6] = r10 * m02 + r11 * m12 + r12 * m22;
+    dst[ 7] = r10 * m03 + r11 * m13 + r12 * m23;
+    dst[ 8] = r20 * m00 + r21 * m10 + r22 * m20;
+    dst[ 9] = r20 * m01 + r21 * m11 + r22 * m21;
+    dst[10] = r20 * m02 + r21 * m12 + r22 * m22;
+    dst[11] = r20 * m03 + r21 * m13 + r22 * m23;
+
+    if (m !== dst) {
+      dst[12] = m[12];
+      dst[13] = m[13];
+      dst[14] = m[14];
+      dst[15] = m[15];
+    }
+
+    return dst;
+  },
+
+  /**
+   * Makes a scale matrix
+   * @param {number} sx x scale.
+   * @param {number} sy y scale.
+   * @param {number} sz z scale.
+   * @param {Matrix4} [dst] optional matrix to store result
+   * @return {Matrix4} dst or a new matrix if none provided
+   * @memberOf module:webgl-3d-math
+   */
+  scaling: function (sx: number, sy: number, sz: number, dst: Matrix4) {
+    dst = dst || new MatType(16);
+
+    dst[ 0] = sx;
+    dst[ 1] = 0;
+    dst[ 2] = 0;
+    dst[ 3] = 0;
+    dst[ 4] = 0;
+    dst[ 5] = sy;
+    dst[ 6] = 0;
+    dst[ 7] = 0;
+    dst[ 8] = 0;
+    dst[ 9] = 0;
+    dst[10] = sz;
+    dst[11] = 0;
+    dst[12] = 0;
+    dst[13] = 0;
+    dst[14] = 0;
+    dst[15] = 1;
+
+    return dst;
+  },
+
+  /**
+   * Multiply by a scaling matrix
+   * @param {Matrix4} m matrix to multiply
+   * @param {number} sx x scale.
+   * @param {number} sy y scale.
+   * @param {number} sz z scale.
+   * @param {Matrix4} [dst] optional matrix to store result
+   * @return {Matrix4} dst or a new matrix if none provided
+   * @memberOf module:webgl-3d-math
+   */
+  scale: function(m:Matrix4, sx: number, sy: number, sz: number, dst: Matrix4) {
+    // This is the optimized version of
+    // return multiply(m, scaling(sx, sy, sz), dst);
+    dst = dst || new MatType(16);
+
+    dst[ 0] = sx * m[0 * 4 + 0];
+    dst[ 1] = sx * m[0 * 4 + 1];
+    dst[ 2] = sx * m[0 * 4 + 2];
+    dst[ 3] = sx * m[0 * 4 + 3];
+    dst[ 4] = sy * m[1 * 4 + 0];
+    dst[ 5] = sy * m[1 * 4 + 1];
+    dst[ 6] = sy * m[1 * 4 + 2];
+    dst[ 7] = sy * m[1 * 4 + 3];
+    dst[ 8] = sz * m[2 * 4 + 0];
+    dst[ 9] = sz * m[2 * 4 + 1];
+    dst[10] = sz * m[2 * 4 + 2];
+    dst[11] = sz * m[2 * 4 + 3];
+
+    if (m !== dst) {
+      dst[12] = m[12];
+      dst[13] = m[13];
+      dst[14] = m[14];
+      dst[15] = m[15];
+    }
+
+    return dst;
+  },
+};
+
+
