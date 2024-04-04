158e8b6029eedd86fbe6ba35679a49e4845263a2
razzanYoni <13521087@mahasiswa.itb.ac.id>
2024-04-04 22:50:59 +0700

    feat: add calculation polygon center

--- src/models/polygon-model.ts
+++ src/models/polygon-model.ts
@@ -31,6 +31,46 @@ export class PolygonModel extends BaseModel {
         this.convexHull();
     }
 
+    public override getCenter(): Coordinates {
+        const numOfTriangles = this.positionBuffer.len - 2;
+        let centerOfTrianglesX: number[] = []
+        let centerOfTrianglesY: number[] = []
+        let areaOfTriangles: number[] = []
+
+        for (let index = 0; index < numOfTriangles; index++) {
+            const x1 = this.positionBuffer.data[0]
+            const y1 = this.positionBuffer.data[1]
+            const x2 = this.positionBuffer.data[(index + 1) * 4]
+            const y2 = this.positionBuffer.data[(index + 1) * 4 + 1]
+            const x3 = this.positionBuffer.data[(index + 2) * 4]
+            const y3 = this.positionBuffer.data[(index + 2) * 4 + 1]
+
+            const area = 0.5 * (x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2))
+            const x = (x1 + x2 + x3) / 3
+            const y = (y1 + y2 + y3) / 3
+
+            areaOfTriangles.push(area)
+            centerOfTrianglesX.push(x)
+            centerOfTrianglesY.push(y)
+        }
+
+        let sumWeightedX = 0;
+        let sumWeightedY = 0;
+        let polygonArea = 0;
+        for (let index = 0; index < centerOfTrianglesX.length; index++) {
+            sumWeightedX += centerOfTrianglesX[index] * areaOfTriangles[index]
+            sumWeightedY += centerOfTrianglesY[index] * areaOfTriangles[index]
+            polygonArea += areaOfTriangles[index]
+        }
+        return new Coordinates(
+            sumWeightedX / polygonArea,
+            sumWeightedY / polygonArea,
+            0,
+            1
+        )
+
+    }
+
     private convexHull(): void {
         const [resPos, resCol] = hull(this.positionBuffer, this.colorBuffer);
         this.positionBuffer = resPos;
