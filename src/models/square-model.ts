733c537ef4a1288a6b73b43cdc43c91175cdc486
Fakhrimm <13521045@mahasiswa.itb.ac.id>
2024-04-05 21:53:12 +0700

    feat: update square model behavior

--- src/models/square-model.ts
+++ src/models/square-model.ts
@@ -11,24 +11,26 @@ export class SquareModel extends BaseModel {
         if (points.length != 2) throw Error("Points in square is not 2")
         this.type = ModelType.SQUARE
 
-        const hLen = Math.abs(points[0].x - points[1].x)
-        const vLen = Math.abs(points[0].y - points[1].y)
-        const offset = hLen > vLen ? vLen : hLen
+        const center = new Coordinates((points[0].x + points[1].x) / 2, (points[0].y + points[1].y) / 2)
 
-        const pivot = new Coordinates(
-            points[1].x > points[0].x ? points[0].x + offset : points[0].x - offset,
-            points[1].y > points[0].y ? points[0].y + offset : points[0].y - offset
-        )
+        const vector = new Coordinates(points[1].x - points[0].x, points[1].y - points[0].y)
+
+        const theta = Math.atan2(vector.y, vector.x) - Math.PI / 4
+
+        const translateRotate = m4.translate(m4.zRotate(m4.translation(center.x, center.y, 0),-theta), -center.x, -center.y, 0);
+        const initVertex = m4.multiply4x1(translateRotate, points[0].getComponents());
+        const endVertex = m4.multiply4x1(translateRotate, points[1].getComponents());
 
         this.positionBuffer = new BufferInfo(
             4,
             [
-                points[0].x, points[0].y, 0, 1,
-                points[0].x, pivot.y, 0, 1,
-                pivot.x, pivot.y, 0, 1,
-                pivot.x, points[0].y, 0, 1
+                initVertex[0], initVertex[1], 0, 1,
+                initVertex[0], endVertex[1], 0, 1,
+                endVertex[0], endVertex[1], 0, 1,
+                endVertex[0], initVertex[1], 0, 1
             ]
         )
+
         this.colorBuffer = new BufferInfo(
             4,
             [
@@ -38,6 +40,7 @@ export class SquareModel extends BaseModel {
                 0.5, 0.5, 0.5, 1
             ]
         )
+        this.z_rotation = theta;
     }
     public override moveVertex(index: number, targetX: number, targetY: number) {
         // index = 2;
@@ -46,32 +49,10 @@ export class SquareModel extends BaseModel {
         const pivotIndex = 0
 
         const pivot = this.getBufferData(BufferType.POSITION, true)[pivotIndex];
-        console.log("pivotIndex", pivotIndex);
-        console.log("pivot", pivot);
 
         const target = new Coordinates(targetX, targetY);
-        console.log("target", target);
 
-        const center = new Coordinates((pivot.x + target.x) / 2, (pivot.y + target.y) / 2);
-
-        const translateRotate = m4.translate(m4.zRotate(m4.translation(center.x, center.y, 0),-this.z_rotation), -center.x, -center.y, 0);
-
-        const movedPivot = m4.multiply4x1(translateRotate, pivot.getComponents());
-        console.log("movedPivot", movedPivot);
-        const movedTarget = m4.multiply4x1(translateRotate, target.getComponents());
-        console.log("movedTarget", movedTarget);
-
-        const newModel = new SquareModel([new Coordinates(movedPivot[0], movedPivot[1]), 
-                                             new Coordinates(movedTarget[0], movedTarget[1])]);
-
-        console.log("newModel", newModel.getBufferData(BufferType.POSITION, true));
-        const translateRotateReverse =  m4.translate(m4.zRotate(m4.translation(center.x, center.y, 0), this.z_rotation), -center.x, -center.y, 0);
-        console.log("translateRotateReversePivot", m4.multiply4x1(translateRotateReverse, movedPivot));
-        console.log("translateRotateReverseTarget", m4.multiply4x1(translateRotateReverse, movedTarget));
-
-        newModel.z_rotation = this.z_rotation;
-        newModel.x_translation = 0
-        newModel.y_translation = 0
+        const newModel = new SquareModel([pivot, target]);
 
         return newModel;
     }
